3f9ce903777d3cdfa05c4efe8453650a9371a398
tieugene <ti.eugene@gmail.com>
2023-01-27 02:00:53 +0300

    Flush dox

--- TODO.md
+++ TODO.md
@@ -1,29 +1,32 @@
 # TODO
 
 ## &hellip;
+- [ ] 0x72 SetDateTime
 - [ ] Fiscal-CS Activity Diagram
-- [ ] Extend commands #2:
-  + [ ] Set:
-    - [ ] 0x72 SetDateTime
-    - [ ] 0x40 ResetMLM
-  + [ ] GetDocByNum #2 (&times;2&hellip;6)
-    - [ ]  3: Receipt (5.3)
-    - [ ]  6: FSCloseRpt (5.6?)
-    - [ ] 21: SattleRpt (5.7)
-    - [ ] 31: CorReceipt
-    - [ ]  4: BSO
-    - [ ] 41: CorBSO
-    - [ ]  7: OpConfirm
-  + [ ] Sequences:
-    + [ ] CancelDoc
-    + [ ] Session
-    + [ ] Receipt
-    + [ ] CorrReceipt
+- [ ] Extend cmd #2 (GetDocByNum #2 (&times;2&hellip;6)):
+  - [ ]  ~~3: Receipt (5.3)~~
+  - [ ]  6: FSCloseRpt (5.6?)
+  - [ ] 21: SattleRpt (5.7)
+  - [ ] 31: CorReceipt
+  - [ ]  4: BSO
+  - [ ] 41: CorBSO
+  - [ ]  7: OpConfirm
+- [ ] Extend cmd #3 (Sequences, (15)):
+    + [ ] CancelDoc (1)
+    + [ ] Session Open(2)/Close(2)
+    + [ ] Receipt (6)
+    + [ ] CorrReceipt (4)
++ [ ] Extend cmd #4 (Maintain):
+  - [ ] 0x40 ResetMLM
+  - [ ] Register
+  - [ ] ReRegister
+  - [ ] FSClose
 - [ ] Mk online tests (w/ live FRs)
 - [ ] Async
 - [ ] Wrap exceptions (all => `exc.Kit&hellip(e)`)
 - [ ] Packaging (`requirements.txt`, `setup.{py,cfg}`, `kitfr.spec`, `?MANIFEST.in`, `?tox.*`)
 - [ ] Try: 2+ &times; Cmd (sequence) @ 1 connection
+- [ ] FR: State diagramm
 - [ ] Maybe: flag._Flags: metaclass
 
 ## CLI:
--- kitfr/const.py
+++ kitfr/const.py
@@ -15,30 +15,30 @@ class _IEnumPrintable(enum.IntEnum):
 @enum.unique
 class IEnumCmd(enum.IntEnum):
     """Commands."""
-    GetDeviceStatus = 0x01  # [Info] Get status, ~args~
-    GetDeviceModel = 0x04  # [Info], ~args~
-    GetStorageStatus = 0x08  # [Info], ~args~
-    GetRegisterParms = 0x0A  # [Info]
-    CancelDoc = 0x10  # [RegFS]
-    SendReceiptAutomatNum = 0x1F  # [Receipt]
-    SessionOpenStart = 0x21  # [Session]
-    SessionOpen = 0x22  # [Session]
-    ReceiptOpen = 0x23  # [Receipt]
-    ReceiptClose = 0x24  # [Receipt]
-    CorrReceiptOpen = 0x25  # [CorRcpt]
-    CorrReceiptClose = 0x26  # [CorRcpt]
-    SessionCloseStart = 0x29  # [Session]
-    SessionClose = 0x2A  # [Session]
-    SendReceiptPos = 0x2B  # [Receipt]
-    SendReceiptAgent = 0x2C  # [Receipt]
-    SendReceiptPay = 0x2D  # [Receipt]
-    SendCorrReceiptData = 0x2E  # [CorRcpt]
-    GetDocByNum = 0x30  # [Archive]
-    SendCorrReceiptAutomatNum = 0x3F  # [CorRcpt]
+    GetDeviceStatus = 0x01            # ✓ [Info] Get status
+    GetDeviceModel = 0x04             # ✓ [Info]
+    GetStorageStatus = 0x08           # ✓ [Info]
+    GetRegisterParms = 0x0A           # ✓ [Info]
+    CancelDoc = 0x10                  # [RegFS]
+    ReceiptSendAutomatNum = 0x1F      # [Receipt]
+    SessionOpenStart = 0x21           # [Session]
+    SessionOpen = 0x22                # [Session]
+    ReceiptOpen = 0x23                # [Receipt]
+    ReceiptClose = 0x24               # [Receipt]
+    CorrReceiptOpen = 0x25            # [CorRcpt]
+    CorrReceiptClose = 0x26           # [CorRcpt]
+    SessionCloseStart = 0x29          # [Session]
+    SessionClose = 0x2A               # [Session]
+    ReceiptSendPos = 0x2B             # [Receipt]
+    ReceiptSendAgent = 0x2C           # [Receipt]
+    ReceiptSendPay = 0x2D             # [Receipt]
+    CorrReceiptSendData = 0x2E        # [CorRcpt]
+    GetDocByNum = 0x30                # … [Archive]
+    CorrReceiptSendAutomatNum = 0x3F  # [CorRcpt]
     # ResetMGM = 0x40
-    GetOFDXchgStatus = 0x50  # [Info]
-    SetDateTime = 0x72  # [Settings]
-    GetDateTime = 0x73  # [Settings]
+    GetOFDXchgStatus = 0x50           # ✓ [Info]
+    SetDateTime = 0x72                # [Settings]
+    GetDateTime = 0x73                # ✓ [Settings]
     Restart = 0xEF
 
 
